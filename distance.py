fb10015a062e476beac4d574a0d862cb43224610
mik0why <mikowhy72@gmail.com>
2019-11-12 10:02:58 -0500

    Fixed problem with sum, working on the distance category

---
+++ distance.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+distance = 0
+cc = {} #dictionary country, capital
+#premise: for each country, compute the distance to the capital
+#https://www.dystans.org/route.json?stops=Gdansk|NewYork
+
+
+cc["Germany"] ="Berlin"
+#nah not adding manually, scrape from some website
+#get 
+print(cc.get("Germany"))
+
+
+def get_country_capital(table):
+	url = "https://www.boldtuesday.com/pages/alphabetical-list-of-all-countries-and-capitals-shown-on-list-of-countries-poster"
+	response = requests.get(url) #three crucial lines for BeautifulSoup to work
+	html = response.content
+	soup = BeautifulSoup(html, 'lxml')
+	table = soup.find('table') #, attrs = {"class" : "youarehere"})
+	rows = table.findAll('tr')
+
+	for row in rows:
+		entry = row.text.splitlines()
+		country = entry[1].encode('utf-8')
+		capital = entry[2].encode('utf-8')
+		cc[country] = capital
+
+get_country_capital(cc)
+print cc
